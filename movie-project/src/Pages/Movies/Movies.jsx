73a7f15a5d452cce9bd5f4926b5f5385eae4fac5
Elvin <elvincabbarl@gmail.com>
2022-07-07 23:54:58 +0400

    filter updated

--- movie-project/src/Pages/Movies/Movies.jsx
+++ movie-project/src/Pages/Movies/Movies.jsx
@@ -2,21 +2,56 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CustomPagination from "../../components/pagination/CustomPagination";
 import SingleContent from "../../components/SingleContent/SingleContent";
-import FilterMovie from "../../components/FilterMovie";
 
 const Movies = () => {
   const [page, setPage] = useState(1);
   const [content, setContent] = useState([]);
+  const [filteredData, setfilteredData] = useState([]);
   const [numOfPages, setnumOfPages] = useState();
-  const [selectedGenres , setSelectedGenres] = useState([])
-  const [genres , setGenres] = useState([])
+
   // SORTING STATE
   const [sort, setSort] = useState("Select");
 
-  const handleSort = (e) => {
-    setSort(e.target.value);
+  
+  const filteredMovie = () => {
+    if (sort === "Select") {
+      return content.map((item) => (
+        <SingleContent
+          key={item.id}
+          id={item.id}
+          poster={item.poster_path}
+          date={item.first_air_date || item.release_date}
+          media_type="movie"
+          vote_average={item.vote_average}
+          title={item.title || item.name}
+        />
+      ));
+    } else if (sort === "IMDB(asc)") {
+      return filteredData.map((item) => (
+        <SingleContent
+          key={item.id}
+          id={item.id}
+          poster={item.poster_path}
+          date={item.first_air_date || item.release_date}
+          media_type="movie"
+          vote_average={item.vote_average}
+          title={item.title || item.name}
+        />
+      ));
+    } else if (sort === "Year(asc)") {
+      return filteredData.map((item) => (
+        <SingleContent
+          key={item.id}
+          id={item.id}
+          poster={item.poster_path}
+          date={item.first_air_date || item.release_date}
+          media_type="movie"
+          vote_average={item.vote_average}
+          title={item.title || item.name}
+        />
+      ));
+    }
   };
-
 
   const fetchMovies = async () => {
     const { data } = await axios.get(
@@ -26,31 +61,64 @@ const Movies = () => {
     setContent(data.results);
     setnumOfPages(data.total_pages);
   };
+  const handleSort = (e) => {
+    setSort(e.target.value);
+    if (e.target.value === "IMDB(asc)") {
+      setfilteredData(
+        content.sort(function (a, b) {
+          return a.vote_average - b.vote_average;
+        })
+      );
+    } else if (e.target.value === "Year(asc)") {
+      setfilteredData(
+        content.sort(function (a, b) {
+          return (
+            parseInt(a.release_date.split("-")[0]) -
+            parseInt(b.release_date.split("-")[0])
+          );
+        })
+      );
+    }
+  };
+
+  console.log(filteredData);
 
   useEffect(() => {
     fetchMovies();
   }, [page]);
 
-
-
   return (
     <>
       <span className="pageTitle">Movies</span>
-      <FilterMovie setPage={setPage} page={page} handleSort={handleSort} />
-      <hr />
-      <div className="trending">
-        {content && content.map((item) => (
-        <SingleContent
-          key={item.id}
-          id={item.id}
-          poster={item.poster_path}
-          date={item.first_air_date || item.release_date}
-          media_type="movie"
-          vote_average={(item.vote_average >8) ? item.vote_average : ''}
-          title={item.title || item.name}
-        />
-      ))}
+      {/* <FilterMovie setPage={setPage} page={page} handleSort={handleSort} /> */}
+      <div>
+        <form style={{ marginBottom: "20px" }} action="">
+          <label style={{ fontSize: "20px" }} htmlFor="">
+            FILTER MOVIE&nbsp;&nbsp;&nbsp;
+          </label>
+
+          <select
+            style={{
+              padding: "10px 15px",
+              fontFamily: "Montserrat",
+              textTransform: "uppercase",
+              outline: "none",
+            }}
+            name=""
+            id="filter"
+            defaultValue={"Select"}
+            onChange={handleSort}
+          >
+            <option value="Select">Select</option>
+            <option value="IMDB(asc)">IMDB(asc)</option>
+            <option value="IMDB(des)">IMDB(des)</option>
+            <option value="Year(asc)">Year(asc)</option>
+            <option value="Year(des)">Year(des)</option>
+          </select>
+        </form>
       </div>
+      <hr />
+      <div className="trending">{filteredMovie()}</div>
       {numOfPages > 1 && (
         <CustomPagination setPage={setPage} numOfPAges={numOfPages} />
       )}
